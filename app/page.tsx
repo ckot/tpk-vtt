3d6d935b08e27e1e0bc9c24b949efef18694e42f
ckot <scott.t.silliman@gmail.com>
2024-04-08 18:23:02 -0400

    switched styling from css modules to tailwind

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
 import Image from "next/image";
-import styles from "./page.module.css";
+// import styles from "./page.module.css";
 import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className={styles.main}>
+    <main className="w-full">
       <header>
         <h1>Welcome to TPK VTT</h1>
       </header>
