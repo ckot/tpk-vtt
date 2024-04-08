c26897da5815ac4309505618acfdc68b63624314
ckot <scott.t.silliman@gmail.com>
2024-04-08 18:46:46 -0400

    initial commit

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,13 @@
+
+import Header from "@/app/ui/header"
+
+export default async function Page() {
+
+    return (
+        <main className="w-full">
+            <Header title="Dashboard" />
+            <div>Your games</div>
+            <div>Your profile</div>
+        </main>
+    )
+}
