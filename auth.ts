17ce16b52695d1ad8988c5f140db07f8c443a95f
ckot <scott.t.silliman@gmail.com>
2024-04-24 11:18:11 -0400

    now simply imports the authConfig and uses it

--- auth.ts
+++ auth.ts
@@ -1,41 +1,43 @@
 import NextAuth from 'next-auth';
-import Credentials from 'next-auth/providers/credentials';
 import { authConfig } from './auth.config';
-import { z } from 'zod';
-import { sql } from '@vercel/postgres';
-import type { User } from '@/app/lib/definitions';
-import bcrypt from 'bcrypt';
 
-async function getUser(email: string): Promise<User | undefined> {
-  try {
-    const user = await sql<User>`SELECT * FROM users WHERE email=${email}`;
-    return user.rows[0];
-  } catch (error) {
-    console.error('Failed to fetch user:', error);
-    throw new Error('Failed to fetch user.');
-  }
-}
+export const { handlers: {GET, POST}, auth, signIn, signOut } = NextAuth(authConfig);
 
-export const { auth, signIn, signOut } = NextAuth({
-  ...authConfig,
-  providers: [
-    Credentials({
-      async authorize(credentials) {
-        const parsedCredentials = z
-          .object({ email: z.string().email(), password: z.string().min(6) })
-          .safeParse(credentials);
 
-        if (parsedCredentials.success) {
-          const { email, password } = parsedCredentials.data;
-          const user = await getUser(email);
-          if (!user) return null;
-          const passwordsMatch = await bcrypt.compare(password, user.password);
-          if (passwordsMatch) return user
-        }
 
-        console.log('Invalid credentials');
-        return null;
-      },
-    }),
-  ],
-});
+// export default NextAuth({
+//   pages: {
+//     signIn: '/'
+//   },
+//   providers: [
+//     CredentialsProvider({
+//       id: 'credentials',
+//       // The name to display on the sign in form (e.g. "Sign in with...")
+//       name: "Credentials",
+//       // `credentials` is used to generate a form on the sign in page.
+//       // You can specify which fields should be submitted, by adding keys to the `credentials` object.
+//       // e.g. domain, username, password, 2FA token, etc.
+//       // You can pass any HTML attribute to the <input> tag through the object.
+//       credentials: {
+//         email: { label: "Email", type: "string", placeholder: "foo@example.com" },
+//         password: {  label: "Password", type: "password" }
+//       },
+//       async authorize(credentials, req) {
+//         // Add logic here to look up the user from the credentials supplied
+//         // const user = { id: "1", name: "J Smith", email: "jsmith@example.com" }
+//         const user = await getUser(credentials.email)
+//         if (!user) {
+//           // Any object returned will be saved in `user` property of the JWT
+//           return null
+//         }
+
+//         else {
+//           // If you return null then an error will be displayed advising the user to check their details.
+//           return null
+
+//           // You can also Reject this callback with an Error thus the user will be sent to the error page with the error message as a query parameter
+//         }
+//       }
+//     })
+// ]
+// })
