0bfd0782669dac58d0609d2d26b23d5fedc5d792
ckot <scott.t.silliman@gmail.com>
2024-04-24 11:07:11 -0400

    removed commented out kruft

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,7 +1,9 @@
 
 import Header from "@/app/ui/header"
+// import { signOut } from "@/auth"
 
 export default async function Page() {
+    // await signOut();
 
     return (
         <main className="w-full">
